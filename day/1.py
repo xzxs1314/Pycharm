716bfd7700861b27ea7dd66163d6f3b42f32d38a
xzxs1314 <2879959563>
2016-10-14 15:17:17 +0800

    brithday1014

--- day/1.py
+++ day/1.py
@@ -1,1 +1,14 @@
-print("hello world")
+import time
+date1=time.strftime('%Y%m%d')
+bridate1=time.strftime('%m%d')
+year=time.strftime('%Y')
+print('今天:',date1)
+date2=input('生日许愿人：你的生辰?\n')
+bridate2=time.strftime('%m%d',time.strptime(date2,'%Y%m%d'))
+briyear=time.strftime('%Y',time.strptime(date2,'%Y%m%d'))
+nl=int(year)-int(briyear)
+print()
+if bridate1==bridate2:
+    print('生日许愿人：祝你%d周岁生日快乐!' % nl)
+else:
+    print('今天不是你生日')
